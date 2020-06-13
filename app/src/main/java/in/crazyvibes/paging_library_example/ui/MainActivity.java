764485b6dcf2238ce9564b6bee403fc0ffc5be81
crazyvibes <bkm123r@gmail.com>
2020-06-13 22:55:22 +0530

    Initial commit

---
+++ app/src/main/java/in/crazyvibes/paging_library_example/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package in.crazyvibes.paging_library_example.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import in.crazyvibes.paging_library_example.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
